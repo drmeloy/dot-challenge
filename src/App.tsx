641ffcc1329baeeced9380bf965093f7caf9fc35
unknown <drmeloy@gmail.com>
2023-09-30 13:51:43 -0700

    dot challenge

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState, type MouseEvent } from 'react';
 import './App.css';
 
+type Dot = {
+  x: number;
+  y: number;
+}
+
 function App() {
+  const [dots, setDots] = useState<Dot[]>([]);
+  const [cache, setCache] = useState<Dot[]>([])
+
+  const drawDot = (e: MouseEvent) => {
+    const { clientX, clientY } = e;
+    setDots([...dots, { x: clientX, y: clientY }]);
+  }
+
+  const undo = () => {
+    if (dots.length) {
+      const newDots = [...dots];
+      const takeAwayDot = newDots.pop() as Dot;
+      setDots(newDots);
+      setCache([...cache, takeAwayDot])
+    }
+  }
+
+  const redo = () => {
+    if (cache.length) {
+      const newCache = [...cache];
+      const returnDot = newCache.pop() as Dot;
+      setCache(newCache);
+      setDots([...dots, returnDot])
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='buttons-container'>
+        <button onClick={undo}>Undo</button>
+        <button onClick={redo}>Redo</button>
+      </div>
+      <div className='click-zone' onClick={drawDot}>
+        {dots.map((dot) => (
+        <div key={`${dot.x}-${dot.y}`} className='dot' style={{ left: dot.x, top: dot.y }}/>
+        ))}
+      </div>
     </div>
   );
 }
